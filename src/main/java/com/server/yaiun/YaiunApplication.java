4d3dc695929a285a762c44ba6bf28e3e39738be9
jianghaoglo <jianghaoglo@qq.com>
2021-09-14 11:55:19 +0800

    搭建

---
+++ src/main/java/com/server/yaiun/YaiunApplication.java
@@ -0,0 +1,13 @@
+package com.server.yaiun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaiunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaiunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/yaiun/YaiunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.yaiun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaiunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
